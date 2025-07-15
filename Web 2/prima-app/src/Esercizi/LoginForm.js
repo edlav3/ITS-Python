3d536e9eca5306603bace896f2dfdba6b32f4fe2
edlav3 <valentinie408@gmail.com>
2025-07-15 16:42:02 +0200

    update

--- Web 2/prima-app/src/Esercizi/LoginForm.js
+++ Web 2/prima-app/src/Esercizi/LoginForm.js
@@ -1,46 +1,81 @@
-import { useState } from "react";
+import React from 'react'
+import { useState } from 'react'
 
 const LoginForm = () => {
-  const [cognome, setCognome] = useState("");
-  const [nome, setNome] = useState("");
-  const gestioneDati = (e) => {
-    e.preventDefault();
-    console.log(e);
-    if (nome && cognome){
-        setNome("")
-        setCognome("")
-    }else{
-        alert("Inserisci tutti i campi")
-    }
-  };
+    const [input, setInput] = useState({ // rinominato da persone a input
+        nome: "",
+        cognome: ""
+    });
 
-  return (
-    <div className="container">
-      <form className="row g-3" onSubmit={gestioneDati}>
-        <div className="col-md-6">
-          <label htmlFor="nome">Nome</label>
-          <input
-            type="text"
-            id="nome"
-            className="form-control"
-            value={nome}
-            onChange={(event) => setNome(event.target.value)}
-          ></input>
+    const [persone, setPersone] = useState([]); // persone ora è un array
+
+    const gestioneDati = (e) => {
+        e.preventDefault();
+        if (input.nome && input.cognome) {
+            setPersone([
+                ...persone,
+                { ...input }
+            ]);
+            setInput({ nome: "", cognome: "" }); // reset campi
+        } else {
+            alert("Inserisci tutti i dati");
+        }
+        console.log(e);
+    };
+
+    const handlerChange = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value }); // corretto e.targetvalue -> e.target.value
+    };
+
+    const animale = {
+        "razza": "gatto",
+        "zampe": "4"
+    };
+    let strRazza = "razza";
+    const animale2 = {
+        ...animale,
+        [strRazza]: "cane"
+    };
+    console.log(animale2.razza);
+
+    return (
+        <div className="container">
+            <form className="row g-3" onSubmit={gestioneDati}>
+                <div className="col-md-6">
+                    <label htmlFor="nome">Nome</label>
+                    <input
+                        type="text"
+                        id="nome"
+                        className="form-control"
+                        required
+                        name="nome"
+                        value={input.nome}
+                        onChange={handlerChange}
+                    ></input>
+                </div>
+                <div className="col-md-6">
+                    <label htmlFor="cognome">Cognome</label>
+                    <input
+                        type="text"
+                        id="cognome"
+                        className="form-control"
+                        required
+                        name="cognome"
+                        value={input.cognome}
+                        onChange={handlerChange}
+                    ></input>
+                </div>
+                <button className="btn btn-success">Login</button>
+            </form><br></br>
+            <div>
+                {
+                    persone.map((p, index) => {
+                        return (<p key={index}>{p.nome} {p.cognome}</p>)
+                    })
+                }
+            </div>
         </div>
-        <div className="col-md-6">
-          <label htmlFor="cognome">Cognome</label>
-          <input
-            type="text"
-            id="cognome"
-            value={cognome}
-            onChange={(event) => setCognome(event.target.value)}
-          ></input>
-        </div>
+    )
+}
 
-        <button className="btn btn-success">Login</button>
-      </form>
-    </div>
-  );
-};
-
-export default LoginForm;
+export default LoginForm
