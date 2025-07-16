e7c3a3dc9674e682f41d6acb4789c1652b9bf6e8
edlav3 <valentinie408@gmail.com>
2025-07-16 16:53:38 +0200

    update

--- Progettazione/BaseDati/Accademia/Interrogazione accademia 5.sql
+++ Progettazione/BaseDati/Accademia/Interrogazione accademia 5.sql
@@ -1,4 +1,11 @@
+/* METODOLOGIA
+1) fare prima un select * dalle tabelle che ci interessano;
+2) visualizzare le caratteristiche delle righe che dobbiamo ottenere;
+3) filtrare i risultati con where
+ */
 --1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome ‘Pegasus’?
+-- => occorre nome, data inizio e data fine dei wp e il nome del progetto che si trovano in due
+-- tabelle diverse
 select
     wp.nome,
     wp.inizio,
@@ -13,22 +20,38 @@ where
 --2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno
 --una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
 select distinct
-    p.nome,
-    p.cognome,
-    p.posizione
+    pe.nome,
+    pe.cognome,
+    pe.posizione
 from
-    Persona p,
+    Persona pe,
     Progetto pr,
-    AttivitaProgetto a
+    AttivitaProgetto att
 where
-    a.persona = p.id
-    and a.progetto = pr.id
+    att.persona = pe.id
+    and att.progetto = pr.id
     and pr.nome = 'Pegasus'
 order by
-    cognome desc;
+    pe.cognome desc;
 
 --3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di
 --una attività nel progetto ‘Pegasus’?
+select
+    pers.nome,
+    pers.cognome,
+    pers.posizione
+from
+    Persona pers,
+    AttivitaProgetto ap1,
+    AttivitaNonProgettuale ap2 Progetto prog
+where
+    prog.nome = 'Pegasus'
+    and ap1.persona = = pers.id
+    and ap2.persona = pers.id
+    and ap1.progetto = prog.id
+    and ap2.progetto = prog.id
+    and ap1.id <> ap2.id
+
 --4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
 --fatto almeno una assenza per malattia?
 --5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
@@ -38,12 +61,24 @@ order by
 --7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un impegno per didattica?
 --8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
 --attività progettuali che attività non progettuali?
+select distinct
+    p.nome,
+    p.cognome
+from
+    persona p,
+    AttivitaProgetto ap,
+    AttivitaNonProgettuale anp
+where
+    ap.giorno = anp.giorno
+    and ap.persona = p.id
+    and anp.persona = p.id
+
 --9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
 --attività progettuali che attività non progettuali? Si richiede anche di proiettare il
 --giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di
 --entrambe le attività.
 --10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
---assenti e hanno attività progettuali?
+--assenti e hanno attività pmrogettuali?
 --11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
 --assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, il
 --nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
