0c209152f9eba1cced0532321e6289e4f7ed0903
edlav3 <valentinie408@gmail.com>
2025-07-22 09:58:52 +0200

    update

--- Progettazione/BaseDati/Accademia/Interrogazione accademia 5.sql
+++ Progettazione/BaseDati/Accademia/Interrogazione accademia 5.sql
@@ -1,12 +1,5 @@
-/* METODOLOGIA
-1) fare prima un select * dalle tabelle che ci interessano;
-2) visualizzare le caratteristiche delle righe che dobbiamo ottenere;
-3) filtrare i risultati con where
- */
 --1. Quali sono il nome, la data di inizio e la data di fine dei WP del progetto di nome ‘Pegasus’?
--- => occorre nome, data inizio e data fine dei wp e il nome del progetto che si trovano in due
--- tabelle diverse
-select
+select distinct
     wp.nome,
     wp.inizio,
     wp.fine
@@ -20,23 +13,23 @@ where
 --2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno almeno
 --una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
 select distinct
-    pe.nome,
-    pe.cognome,
-    pe.posizione
+    p.nome,
+    p.cognome,
+    p.posizione
 from
-    Persona pe,
+    Persona p,
     Progetto pr,
-    AttivitaProgetto att
+    AttivitaProgetto a
 where
-    att.persona = pe.id
-    and att.progetto = pr.id
+    a.persona = p.id
+    and a.progetto = pr.id
     and pr.nome = 'Pegasus'
 order by
-    pe.cognome desc;
+    cognome desc;
 
 --3. Quali sono il nome, il cognome e la posizione degli strutturati che hanno più di
 --una attività nel progetto ‘Pegasus’?
-select
+select distinct
     pers.nome,
     pers.cognome,
     pers.posizione
@@ -50,14 +43,42 @@ where
     and ap2.persona = pers.id
     and ap1.progetto = prog.id
     and ap2.progetto = prog.id
-    and ap1.id <> ap2.id
+    and ap1.id <> ap2.id;
 
 --4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
 --fatto almeno una assenza per malattia?
+select distinct
+    pe.nome,
+    pe.cognome,
+    pe.posizione
+from
+    persona pe,
+    Assenza a
+where
+    pe.id = a.persona
+    and pe.posizione = 'Professore Ordinario'
+    and a.tipo = 'Malattia';
+
 --5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
 --fatto più di una assenza per malattia?
+select distinct
+    p.nome,
+    p.cognome,
+    p.posizione
+from
+    Persona p,
+    Assenza a1,
+    Assenza a2
+where
+    p.id = a1.persona
+    and p.id = a2.persona
+    and a1.tipo = 'Malattia'
+    and a2.tipo = 'Malattia'
+    and a1.giorno <> a2.giorno
+    and p.posizione = 'Professore Ordinario';
+
 --6. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno almeno
---un impegno per didattica?
+--un impegno per didattica
 --7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un impegno per didattica?
 --8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
 --attività progettuali che attività non progettuali?
@@ -71,15 +92,70 @@ from
 where
     ap.giorno = anp.giorno
     and ap.persona = p.id
-    and anp.persona = p.id
+    and anp.persona = p.id;
 
 --9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
 --attività progettuali che attività non progettuali? Si richiede anche di proiettare il
 --giorno, il nome del progetto, il tipo di attività non progettuali e la durata in ore di
---entrambe le attività.
+--entrambe le attività
+select distinct
+    p.nome,
+    p.cognome,
+    anp.giorno,
+    anp.tipo,
+    ap.tipo,
+    anp.oreDurata,
+    ap.oreDurata
+from
+    AttivitaNonProgettuale anp,
+    AttivitaProgetto ap,
+    persona p
+where
+    anp.giorno = ap.giorno
+    and anp.persona = p.id
+    and ap.persona = p.id;
+
 --10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
---assenti e hanno attività pmrogettuali?
+--assenti e hanno attività progettuali?
+select distinct
+    pe.nome,
+    pe.cognome
+from
+    Assenza az,
+    AttivitaProgetto att,
+    persona pe
+where
+    pe.id = att.persona
+    and pe.id = az.persona
+    and az.giorno = att.giorno;
+
 --11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
 --assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, il
 --nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
+select distinct
+    pe.nome,
+    pe.cognome,
+    a.giorno,
+    pr.nome,
+    a.tipo,
+    ap.oreDurata
+from
+    Persona pe,
+    Assenza a,
+    AttivitaProgetto ap,
+    Progetto pr
+where
+    pe.id = a.persona
+    and pe.id = ap.persona
+    and a.giorno = ap.giorno
+    and ap.progetto = pr.id;
+
 --12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?
+select distinct
+    wp1.nome
+from
+    wp wp1,
+    wp wp2
+where
+    wp1.nome = wp2.nome
+    and wp1.progetto <> wp2.progetto;
