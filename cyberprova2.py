1d2df1137652b9c0e421bad3cae77514603e68f5
edlav3 <valentinie408@gmail.com>
2026-01-07 13:48:28 +0100

    update

--- cyberprova2.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-challenge = "http://challenge01.root-me.org/web-serveur/ch2/"
-
-parametri = {
-    'User-Agent': 'admin'
-}
-risposta=requests.get(challenge, headers=parametri)
-print(risposta.text)
